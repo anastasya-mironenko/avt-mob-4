ddbae1aeb5e241b7440cb32a56c54cbd9c88586b
Anastasia M <voronina_nastya9@mail,ru>
2018-08-16 10:41:55 +0300

    ID и Not Present

--- JavaAppiumAutomation/src/FirstTest.java
+++ JavaAppiumAutomation/src/FirstTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -37,18 +38,25 @@ public class FirstTest {
         driver.quit();
     }
 
-    @Test
-    public void fistTest()
-    {
-        waitForElementByXpathAndClick("//*[contains(@text, 'Поиск по Википедии')]", "Cannot find search input", 5);
-        waitForElementByXpathAndSendKeys("//*[contains(@text, 'Поиск')]", "Java", "Cannot find 'Язык программирования' topic searching by 'Java'", 5);
-        waitForElementPresentByXpath(
-                "//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Язык программирования']",
-                "Cannot find 'Язык программирования' topic searching by 'Java'",
-                15
-        );
+//    @Test
+//    public void fistTest()
+//    {
+//        waitForElementByXpathAndClick("//*[contains(@text, 'Поиск по Википедии')]", "Cannot find search input", 5);
+//        waitForElementByXpathAndSendKeys("//*[contains(@text, 'Поиск')]", "Java", "Cannot find 'Язык программирования' topic searching by 'Java'", 5);
+//        waitForElementPresentByXpath(
+//                "//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Язык программирования']",
+//                "Cannot find 'Язык программирования' topic searching by 'Java'",
+//                15
+//        );
+//
+//        // System.out.println("first test run");
+//    }
 
-        // System.out.println("first test run");
+    @Test
+    public void testCancelSearch(){
+        waitForElementByIdAndClick("org.wikipedia:id/search_container", "Cannot find search input", 5);
+        waitForElementByIdAndClick("org.wikipedia:id/search_close_btn", "Cannot find X to cancel search", 5);
+        waitForElementNotPresent("org.wikipedia:id/search_close_btn", "X is still present on the page", 5);
     }
 
     private WebElement waitForElementPresentByXpath(String xpath, String error_message, long timeoutInSeconds){
@@ -73,4 +81,26 @@ public class FirstTest {
         element.sendKeys(value);
         return element;
     }
+
+    private WebElement waitForElementPresentById(String id, String error_message, long timeoutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.id(id);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementByIdAndClick(String id, String error_message, long timeoutInSeconds){
+        WebElement element = waitForElementPresentById(id, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private boolean waitForElementNotPresent (String id, String error_message, long timeoutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + '\n');
+        By by = By.id(id);
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
+    }
+
+
 }
